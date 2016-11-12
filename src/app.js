ae00cf7354848672ff23e25134bc83f7ba957090
rizwanahmed19 <rizwan.ahmed19@outlook.com>
2016-11-12 17:05:19 +0500

    Init commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+
+
+$(document).foundation();
+
+import 'style!css!sass!./styles/app.scss';
+
+ReactDOM.render(
+	<p>React Boilerplate3 project</p>, 
+	document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var htmlWebpackPluginConfig = new htmlWebpackPlugin({
+	template: './src/main.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+module.exports = {
+	devtools: 'eval-source-map',
+	entry: [
+		'script!jquery/dist/jquery.min.js',
+		'script!foundation-sites/dist/foundation.min.js',
+		'./src/app.js'
+	],
+	externals: {
+		jquery: 'jQuery'
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			'$': 'jquery',
+			'jQuery': 'jquery'
+		})
+	],
+	output: {
+		path: __dirname + '/public/',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				exclude: /node_modules/,
+				query: {
+					presets: ['react', 'es2015', 'stage-2']
+				} 
+			}
+		]
+	},
+	sassLoader: {
+		includePaths: [
+			path.resolve(__dirname, './node_modules/foundation-sites/scss')
+		]
+	},
+	plugins: [
+		htmlWebpackPluginConfig
+	]
+};
