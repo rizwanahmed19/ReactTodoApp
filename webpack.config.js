571f90834954c2af9d69cbcd93fb692f7dba261b
rizwanahmed19 <rizwan.ahmed19@outlook.com>
2017-01-15 15:32:49 +0500

    correct the foundation path

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
 	devtool: process.env.NODE_ENV === 'production' ? undefined : 'eval-source-map',
 	entry: [
 		'script!jquery/dist/jquery.min.js',
-		'script!foundation-sites/dist/js/foundation.min.js',
+		'script!foundation-sites/dist/foundation.min.js',
 		'./src/app.js'
 	],
 	externals: {
