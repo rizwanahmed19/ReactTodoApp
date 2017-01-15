c3ff80a3427fe5f70c756d9269be3f143e4cd085
rizwanahmed19 <rizwan.ahmed19@outlook.com>
2017-01-15 12:57:59 +0500

    move Firebase creds to env variables

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
 	var config = {
-    apiKey: "AIzaSyDY1CKql03e0wxiOc8D9pL_NE3Civ8JaUQ",
-    authDomain: "my-react-todo.firebaseapp.com",
-    databaseURL: "https://my-react-todo.firebaseio.com",
-    storageBucket: "my-react-todo.appspot.com",
-    messagingSenderId: "1022834878824"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
 	};
 	firebase.initializeApp(config);
 } catch(e){
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+var envFile = require('node-env-file');
 var htmlWebpackPlugin = require('html-webpack-plugin');
 var htmlWebpackPluginConfig = new htmlWebpackPlugin({
 	template: './src/main.html',
@@ -8,6 +9,12 @@ var htmlWebpackPluginConfig = new htmlWebpackPlugin({
 });
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+try {
+	envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
+} catch(e) {
+
+}
 
 module.exports = {
 	devtool: process.env.NODE_ENV === 'production' ? undefined : 'eval-source-map',
@@ -28,6 +35,16 @@ module.exports = {
 		new webpack.optimize.UglifyJsPlugin({
 			compressor: {
 				warnings: false
+			}
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+				API_KEY: JSON.stringify(process.env.API_KEY),
+				AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+				DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+				STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+				MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
 			}
 		})
 	],
